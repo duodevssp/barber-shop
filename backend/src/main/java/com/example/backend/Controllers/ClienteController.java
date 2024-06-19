9821b0117399d8184971e1125637cace30562a0d
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-06-19 11:08:46 -0300

    concluido o controller cliente

--- backend/src/main/java/com/example/backend/Controllers/ClienteController.java
+++ backend/src/main/java/com/example/backend/Controllers/ClienteController.java
@@ -4,16 +4,17 @@ import com.example.backend.Models.ClienteModel;
 import com.example.backend.RecordDto.ClienteRecordDto;
 import com.example.backend.Repositories.ClienteRepository;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ClienteController {
@@ -30,6 +31,37 @@ public class ClienteController {
     @GetMapping("/clientes")
     public ResponseEntity<List<ClienteModel>> getClientes(){
         return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.findAll());
+    }
+
+    @GetMapping("/clientes/{id}")
+    public ResponseEntity<Object> getOneCliente(@PathVariable(value="id") UUID id){
+        Optional<ClienteModel> clienteO = clienteRepository.findById(id);
+        if(clienteO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(clienteO.get());
+    }
+
+    @PutMapping("/clientes/{id}")
+    public ResponseEntity<Object> updateClientes(@PathVariable(value = "id")UUID id,
+                                                 @RequestBody @Valid ClienteRecordDto clienteRecordDto){
+        Optional<ClienteModel> clienteO = clienteRepository.findById(id);
+        if(clienteO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+        }
+        var clienteModel = clienteO.get();
+        BeanUtils.copyProperties(clienteRecordDto, clienteModel);
+        return ResponseEntity.status(HttpStatus.OK).body(clienteRepository.save(clienteModel));
+    }
+
+    @DeleteMapping("clientes/{id}")
+    public ResponseEntity<Object> deleteClientes(@PathVariable(value="id") UUID id){
+        Optional<ClienteModel> clienteO = clienteRepository.findById(id);
+        if(clienteO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não encontrado");
+        }
+        clienteRepository.delete(clienteO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Cliente excluído com sucesso");
     }
 
 }
