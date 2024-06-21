7756f72536d5f5bf35658ded6c6e21037e445184
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-06-21 09:47:59 -0300

    metodos PUT e DELETE para o controller Barbeiro

--- backend/src/main/java/com/example/backend/Controllers/BarbeiroController.java
+++ backend/src/main/java/com/example/backend/Controllers/BarbeiroController.java
@@ -19,25 +19,47 @@ public class BarbeiroController {
     @Autowired
     BarbeiroRepository barbeiroRepository;
 
-    @PostMapping("/barbeiro")
+    @PostMapping("/barbeiros")
     public ResponseEntity<BarbeiroModel> createBarbeiro(@RequestBody @Valid BarbeiroRecordDto barbeiroRecordDto){
         var barbeiroModel = new BarbeiroModel();
         BeanUtils.copyProperties(barbeiroRecordDto, barbeiroModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(barbeiroRepository.save(barbeiroModel));
     }
 
-    @GetMapping("/barbeiro")
+    @GetMapping("/barbeiros")
     public ResponseEntity<List<BarbeiroModel>> getBarbeiroAll(){
         return ResponseEntity.status(HttpStatus.OK).body(barbeiroRepository.findAll());
     }
 
-    @GetMapping("/barbeiro/{id}")
+    @GetMapping("/barbeiros/{id}")
     public ResponseEntity<Object> getOneBarbeiro(@PathVariable(value="id") UUID id){
         Optional<BarbeiroModel> barbeiroO = barbeiroRepository.findById(id);
         if(barbeiroO.isEmpty()){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Barbeiro não encontrado");
         }
         return ResponseEntity.status(HttpStatus.OK).body(barbeiroO.get());
+    }
+
+    @PutMapping("/barbeiros/{id}")
+    public ResponseEntity<Object> updateBarbeiro(@RequestBody @Valid BarbeiroRecordDto barbeiroRecordDto,
+                                                 @PathVariable(value="id") UUID id){
+        Optional<BarbeiroModel> barbeiroO = barbeiroRepository.findById(id);
+        if(barbeiroO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Barbeiro não encontrado");
+        }
+        var barbeiroModel = barbeiroO.get();
+        BeanUtils.copyProperties(barbeiroRecordDto, barbeiroModel);
+        return ResponseEntity.status(HttpStatus.OK).body(barbeiroRepository.save(barbeiroModel));
+    }
+
+    @DeleteMapping("/barbeiros/{id}")
+    public ResponseEntity<Object> deleteBarbeiro(@PathVariable(value="id") UUID id){
+        Optional<BarbeiroModel> barbeiroO = barbeiroRepository.findById(id);
+        if(barbeiroO.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Barbeiro não encontrado");
+        }
+        barbeiroRepository.delete(barbeiroO.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Barbeiro excluído com sucesso");
     }
 
 
